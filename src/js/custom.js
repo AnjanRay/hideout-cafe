816b0757c243e37a0bb1a965fe9e44ea0ecf1491
AnjanRay <www.anjanray.ar@gmail.com>
2018-12-06 18:12:56 +0600

    init

---
+++ src/js/custom.js
@@ -0,0 +1,39 @@
+$('.product-menu').isotope({
+    itemSelector: '.menu-item',
+    layoutMode: 'fitRows',
+  })
+  $('.menu-list ul li').click(function () {
+    console.log('iiiii')
+    $('.menu-list  ul li').removeClass('active');
+    $(this).addClass('active');
+
+    var selector = $(this).attr('data-filter');
+    console.log(selector)
+    $('.product-menu').isotope({
+      filter: selector
+    });
+    return false;
+  });
+
+  $('.main_menu_wrap').singlePageNav({
+    offset: $('.single-page-nav').outerHeight(),
+    threshold: 120,
+    speed: 400,
+    currentClass: 'current',
+    easing: 'swing',
+    filter: ':not(.external)',
+
+    beforeStart: function () {
+      console.log('begin scrolling');
+    },
+    onComplete: function () {
+      console.log('done scrolling');
+    }
+  });
+
+  $('.sidebar-toggle').on("click", function() {
+    $('.fixed-sidebar').addClass('open')
+  });
+  $('.sidebar-close').on("click", function() {
+    $('.fixed-sidebar').removeClass('open')
+  })
